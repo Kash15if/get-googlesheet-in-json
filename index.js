ea736789601a8704bd17cc93b24ac283340d2510
Kash15if <kash15if@yahoo.com>
2020-07-13 14:59:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const {google} = require('googleapis');
+const keys = require('./keys.json');
+
+
+
+
+
+const client = new google.auth.JWT(
+    keys.client_email,                                             //email
+    null,                                                         //keyfile
+    keys.private_key,                                            //key
+    ['https://www.googleapis.com/auth/spreadsheets']            // scopes
+);
+
+
+client.authorize(function(err,tokens){
+
+    if(err)
+        console.log(err)
+    else
+        gsheet(client);
+});
+
+
+async function gsheet(client){
+
+    const gsApi = google.sheets({version: "v4" , auth: client});
+    
+    const opt = {
+        spreadsheetId: '', //enter your spreadsheet api
+        range: 'B1:J25'    //from which to which column
+    }
+
+    data = await gsApi.spreadsheets.values.get(opt);
+
+
+    app.get('/',function(req,res){
+        res.json(data.data.values);
+         });  
+}
+
+
+app.listen(8000);
+
